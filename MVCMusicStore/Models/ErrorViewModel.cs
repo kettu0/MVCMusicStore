5c6c47bfc720c36b7e4d583ff76c80b8bd7e09a7
kettu0 <aneta.kruc@gmail.com>
2022-10-23 16:54:16 +0200

    Dodaj pliki projektów.

---
+++ MVCMusicStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCMusicStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
